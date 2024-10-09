424e9376ae70725d545c728563e2aba99a3cdd08
ShrishtiSingh26 <shrishtisingh0511@gmail.com>
2024-10-09 12:40:31 +0530

    custom cursor effect

--- Alien.js
+++ Alien.js
@@ -519,3 +519,42 @@ fireButton.addEventListener("touchstart", (e) => {
 fireButton.addEventListener("touchend", () => {
   // Logic for stopping fire can be added here if needed
 });
+//cursor trail effect
+const coords = { x: 0, y: 0 };
+        const circles = document.querySelectorAll(".circle");
+        
+        
+        // Assign initial colors to the circles
+        circles.forEach(function (circle) {
+            circle.x = 0;
+            circle.y = 0;
+            
+        });
+
+        window.addEventListener("mousemove", function (e) {
+            coords.x = e.clientX;
+            coords.y = e.clientY;
+            
+        });
+
+        function animateCircles() {
+            let x = coords.x;
+            let y = coords.y;
+
+            circles.forEach(function (circle, index) {
+                circle.style.left = x - 12 + "px";
+                circle.style.top = y - 12 + "px";
+                circle.style.scale=(circles.length - index) / circles.length;
+
+                circle.x = x;
+                circle.y = y;
+
+                const nextCircle = circles[index + 1] || circles[0];
+                x += (nextCircle.x - x) * 0.3;
+                y += (nextCircle.y - y) * 0.3;
+            });
+
+            requestAnimationFrame(animateCircles);
+        }
+
+        animateCircles();
